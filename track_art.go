5777bebc118586d11afe1b2a14cc9187caaa015a
sc <65317624+skateboard@users.noreply.github.com>
2024-09-04 21:25:36 +0400

    Update track_art.go

--- track_art.go
+++ track_art.go
@@ -1,69 +1,24 @@
 package soundcloud
 
 import (
-	"errors"
 	"io"
 	"net/http"
-
-	"github.com/johnreutersward/opengraph"
+	"strings"
 )
 
-func (s *Soundcloud) extractTrackArt() ([]byte, error) {
-	metaData, err := fetchMetaData(s.trackURL)
+func (s *Soundcloud) extractTrackArt(artWorkUrl string) ([]byte, error) {
+	artWorkUrl = strings.ReplaceAll(artWorkUrl, "large", "t500x500")
+
+	res, err := http.Get(artWorkUrl)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	b, err := io.ReadAll(res.Body)
 	if err != nil {
 		return nil, err
 	}
 
-	artwork := extractMetaData(metaData)
-
-	if artwork.ImageURL != "" {
-		res, err := http.Get(artwork.ImageURL)
-		if err != nil {
-			return nil, err
-		}
-		defer res.Body.Close()
-
-		b, err := io.ReadAll(res.Body)
-		if err != nil {
-			return nil, err
-		}
-
-		return b, nil
-	}
-
-	return nil, errors.New("failed to extract track art")
-}
-
-type artwork struct {
-	Title    string
-	ImageURL string
-}
-
-func fetchMetaData(url string) ([]opengraph.MetaData, error) {
-	res, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-
-	md, err := opengraph.Extract(res.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return md, nil
-}
-
-func extractMetaData(md []opengraph.MetaData) artwork {
-	var artwork artwork
-
-	for i := range md {
-		switch md[i].Property {
-		case "title":
-			artwork.Title = md[i].Content
-		case "image":
-			artwork.ImageURL = md[i].Content
-		}
-	}
-
-	return artwork
+	return b, nil
 }
